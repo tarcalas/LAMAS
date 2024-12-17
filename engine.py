11b18f00aecdccc2b6bb0ddc8ead83560f64b97b
tarcalas <90679328+tarcalas@users.noreply.github.com>
2024-12-17 12:03:22 +0100

    Create engine.py
    Added functions that need implementations

---
+++ engine.py
@@ -0,0 +1,17 @@
+#I was thinking of representing the cards as numbers, with N%10 = 0 => A, N%10 = 1 => K, N%10 = 1 =>Q
+#N/10 = 0 => Spades, N/10 = 1 => Hearts, N/10 = 2 => Diamonds, N/10 = 3 =>Clubs 
+#So 0 = Ace of Spades, 31 = King of Clubs, etc.
+
+#Also thinking about the different combinations as integers like 0 = 4 Aces, 1 = 4 Kings, etc.
+
+#calculates the probability of winning from an agents perspective
+def probabilityOfWinning(agentsCards, visibleCards, agentsBestHand):
+  return probability
+
+#deal cards
+def dealCards(deck):
+  return (agent0Cards, agent1Cards, restOfDeck)
+
+#recursive approach is probably the best
+def bestHand(agentsCards, visibleCards, pickedCards):
+  return bestHandValue
