0dfe6f86ee27a71f5e992d0202914d2d80824fe9
Jiri Derks <jiri_derks@hotmail.com>
2025-01-07 11:48:40 +0100

    Added opponent's possible hands to agents and dealing

--- engine.py
+++ engine.py
@@ -64,6 +64,7 @@ class Agent:
     def __init__(self, name):
         self.name = name
         self.cards = []
+        self.possibleOpponentCards = []
 
     def setCards(self, cards):
         self.cards = cards
@@ -74,12 +75,34 @@ class Agent:
     def getName(self):
         return self.name
 
-def main():
-    deck = np.array([0,1,2,10,11,12,20,21,22,30,31,32])
+def calculatePossibleOpponentHands(agentsCards, visibleCards, deck):
+    possibleOpponentCards = []
 
+    for i in range(len(deck)):
+        for j in range(i+1, len(deck)):
+            possibleOpponentCards.append([deck[i], deck[j]])
+
+    possibleOpponentCards = np.array(possibleOpponentCards)
+
+    for i in range(len(agentsCards)):
+        possibleOpponentCards = possibleOpponentCards[np.all(possibleOpponentCards != agentsCards[i], axis=1)]
+
+    for i in range(len(visibleCards)):
+        possibleOpponentCards = possibleOpponentCards[np.all(possibleOpponentCards != visibleCards[i], axis=1)]    
+
+    return possibleOpponentCards
+
+def main():
+
+    #initialize deck and comunity cards
+    deck = np.array([0,1,2,10,11,12,20,21,22,30,31,32])
+    visibleCards = np.array([])
+
+    # initialize agents
     agent0 = Agent("Agent 0")
     agent1 = Agent("Agent 1")
 
+    #deal initial cards
     agent0Cards, agent1Cards, restOfDeck = dealInitialCards(deck)
 
     agent0.setCards(agent0Cards)
@@ -88,8 +111,55 @@ def main():
     print(agent0.getName() + " has " + cardToName(agent0.getCards()[0]) + " and " + cardToName(agent0.getCards()[1]))
     print(agent1.getName() + " has " + cardToName(agent1.getCards()[0]) + " and " + cardToName(agent1.getCards()[1]))
 
-    
+    # calculate possible opponent hands
+    agent0.possibleOpponentCards = calculatePossibleOpponentHands(agent0.getCards(), visibleCards, deck)
+    agent1.possibleOpponentCards = calculatePossibleOpponentHands(agent1.getCards(), visibleCards, deck)
 
+    print(agent0.getName() + "'s opponent has " + str(len(agent0.possibleOpponentCards)) + " possible hands")
+    print(agent1.getName() + "'s opponent has " + str(len(agent1.possibleOpponentCards)) + " possible hands")
+
+    #deal flop
+    for i in range(3):
+        visibleCards = np.append(visibleCards, np.random.choice(restOfDeck))
+        restOfDeck = np.delete(restOfDeck, np.where(restOfDeck == visibleCards[i]))
+
+    print("Flop: " + cardToName(visibleCards[0]) + ", " + cardToName(visibleCards[1]) + ", " + cardToName(visibleCards[2]))
+
+    # calculate possible opponent hands
+    agent0.possibleOpponentCards = calculatePossibleOpponentHands(agent0.getCards(), visibleCards, deck)
+    agent1.possibleOpponentCards = calculatePossibleOpponentHands(agent1.getCards(), visibleCards, deck)
+
+    print(agent0.getName() + "'s opponent has " + str(len(agent0.possibleOpponentCards)) + " possible hands")
+    print(agent1.getName() + "'s opponent has " + str(len(agent1.possibleOpponentCards)) + " possible hands")
+
+    #deal turn
+    visibleCards = np.append(visibleCards, np.random.choice(restOfDeck))
+    restOfDeck = np.delete(restOfDeck, np.where(restOfDeck == visibleCards[3]))
+
+    print("Turn: " + cardToName(visibleCards[3]))
+
+    # calculate possible opponent hands
+    agent0.possibleOpponentCards = calculatePossibleOpponentHands(agent0.getCards(), visibleCards, deck)
+    agent1.possibleOpponentCards = calculatePossibleOpponentHands(agent1.getCards(), visibleCards, deck)
+
+    print(agent0.getName() + "'s opponent has " + str(len(agent0.possibleOpponentCards)) + " possible hands")
+    print(agent1.getName() + "'s opponent has " + str(len(agent1.possibleOpponentCards)) + " possible hands")
+
+    #deal river
+    visibleCards = np.append(visibleCards, np.random.choice(restOfDeck))
+    restOfDeck = np.delete(restOfDeck, np.where(restOfDeck == visibleCards[4]))
+
+    print("River: " + cardToName(visibleCards[4]))
+
+    # calculate possible opponent hands
+    agent0.possibleOpponentCards = calculatePossibleOpponentHands(agent0.getCards(), visibleCards, deck)
+    agent1.possibleOpponentCards = calculatePossibleOpponentHands(agent1.getCards(), visibleCards, deck)
+
+    print(agent0.getName() + "'s opponent has " + str(len(agent0.possibleOpponentCards)) + " possible hands")
+    print(agent1.getName() + "'s opponent has " + str(len(agent1.possibleOpponentCards)) + " possible hands")
+                           
+
+    
 
 
     
