9d8678cd5311a26f846a5342455fb7984d40b93a
Jiri Derks <jiri_derks@hotmail.com>
2025-01-24 00:28:06 +0100

    Seet for Demo

--- engine.py
+++ engine.py
@@ -287,6 +287,8 @@ def recalculatewinningProbability(agent, opponentPercentage, visibleCards, deck)
 
     
 def main():
+    #set random seed for demo
+    np.random.seed(3)
 
     #initialize deck and comunity cards
     deck = np.array([0,1,2,10,11,12,20,21,22,30,31,32])
